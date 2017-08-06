a9eeb21f07a8dd3a52126bd2b50db1c37159a8e9
Naim Jeem <naim36208@gmail.com>
2017-08-07 00:04:11 +0600

    PMA dark theme

---
+++ info.inc.php
@@ -0,0 +1,14 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ * @package    PhpMyAdmin-theme
+ * @subpackage Original
+ */
+
+/**
+ *
+ */
+$theme_name = 'pmadark';
+$theme_full_version = '0.1';
